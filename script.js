ce3d9dc51148d75d23b8bf1545d79fc12bfdcffb
inessa <inessa.boickova@yandex.ru>
2022-05-05 17:07:03 +0300

    add the input data extension to the content width

--- script.js
+++ script.js
@@ -1,55 +1,73 @@
 const val =  document.querySelector('.checklist_input'),
       addBtn = document.querySelector('.checklist_btn'),
       task =  document.querySelectorAll('.checklist_subheader'),
-      message = document.querySelector('.checklist_count'),
+      result = document.querySelector('.wrapper');
       clear = document.querySelector('.checklist_clear'),
       delite = document.querySelectorAll('.checklist_button');
 
-let counter = 0;
+function createCount(count){
+    if (count < 0){
+        count = 0;
+    }
+    result.innerHTML =`<h4 class="checklist_count">You have padding ${count} task</h4>`
+}
 
-addBtn.addEventListener('click',()=> {
+let counter = 0;
+createCount(counter);
+
+val.addEventListener('keydown', function(e) {
+    if (e.keyCode === 13 && (val.value != undefined && val.value != '')) {
+        counter++;
+        addTask(counter - 1);  
+        createCount(counter);
+    }
+});
+
+addBtn.addEventListener('click',(e)=> {
     counter++;
     addTask(counter - 1);  
+    createCount(counter);
 })
 
 clear.addEventListener('click',() => deliteAll());
 
 delite.forEach((item,i)=>{
     item.addEventListener('click', () =>{
-        task[i].innerText= '';
-        
-        if(counter == 0 ){
-            message.innerText= `You have padding 0 task`;
-        }else{
-            counter = counter - 1 ;
-            message.innerText= `You have padding ${counter} task`;
+        counter = counter - 1 ;
+        createCount(counter );
+        for(let i = 0 ; i<= task.length;i++){
+            task[i].innerHTML = task[i + 1].innerHTML;
+            if (i === 2){
+                task[3].innerHTML= task[4].innerHTML;
+            }else if ( i === 3){
+                task[task.length - 1].innerHTML = ``;
+            }  
         }
-        task[i].innerText= task[counter].innerText;
-        
-        if(task[counter].innerText = ''){
-            console.log('dfsdfsdf');
-        }
+       
     })
 })
 
 function addTask(count){
     if(counter > 5){
-        alert('Oh, maybe that is enough for today?');
+       alert('maybe that is it for today ?')
+       counter = 5;
     }else{
-        task[count].innerText= val.value;
-        message.innerText= `You have padding ${count+1} task`;
-        
+        task[count].innerHTML= `<h3> ${val.value}</h3>`;
+        val.value = '';
     }
-    val.value = '';
 }
 
 function deliteAll(){
-   for(let i = 0 ;i<= 5;i++){
-        task[i].innerText= '';
-        message.innerText= `You have padding 0 task`;
-        counter = 0;
-   } 
+    counter = 0;
+    createCount(counter);
+    for(let i = 0 ; i <= task.length ; i++){
+        task[i].innerHTML= ``;
+    } 
 }
+
+
+
+
 
 
 
