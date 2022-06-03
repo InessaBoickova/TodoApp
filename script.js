1195e4fe54db7b4d38e48c3e770df02d599f08f5
Inessa <inessa.boickova@andex.by>
2022-06-03 19:54:00 +0300

    implementation of adding elements

--- script.js
+++ script.js
@@ -1,71 +1,59 @@
 const val =  document.querySelector('.checklist_input'),
       addBtn = document.querySelector('.checklist_btn'),
-      task =  document.querySelectorAll('.checklist_subheader'),
-      result = document.querySelector('.wrapper');
-      clear = document.querySelector('.checklist_clear'),
-      delite = document.querySelectorAll('.checklist_button');
-
-function createCount(count){
-    if (count < 0){
-        count = 0;
-    }
-    result.innerHTML =`<h4 class="checklist_count">You have padding ${count} task</h4>`
-}
+      task =  document.querySelector('.checklist_task'),
+      result = document.querySelector('.wrapper'),
+      clear = document.querySelector('.checklist_clear');
 
 let counter = 0;
+function createCount(count){
+    result.innerHTML =`<h4 class="checklist_count">You have padding ${count} task</h4>`
+}
 createCount(counter);
+
+function addTask(count){
+    let wrap = document.createElement('div');
+    wrap.classList.add("checklist_case");
+    wrap.id = count;
+    task.appendChild(wrap);
+    wrap.innerHTML = `
+        <div class="checklist_subheader">${val.value}</div>
+        <button class="checklist_button"> </button>
+    `
+    val.value = '';
+    let delite = document.querySelectorAll('.checklist_button');
+    delite.forEach((item)=>{
+        item.addEventListener('click',()=>{
+            item.parentNode.remove();
+            createCount(task.childElementCount);  
+        }) 
+    })   
+}  
+
+function deliteAll(){
+    for(let i = 1; i <= counter ; i++){
+        document.getElementById(`${i}`).remove();
+    } 
+    counter = 0;
+    createCount(counter);
+}
 
 val.addEventListener('keydown', function(e) {
     if (e.keyCode === 13 && (val.value != undefined && val.value != '')) {
         counter++;
-        addTask(counter - 1);  
+        addTask(counter);  
         createCount(counter);
     }
 });
 
+clear.addEventListener('click',() => deliteAll());
+
 addBtn.addEventListener('click',(e)=> {
     if(val.value != undefined && val.value != ''){
         counter++;
-        addTask(counter - 1);  
+        addTask(counter);  
         createCount(counter);
     }
 })
-
-clear.addEventListener('click',() => deliteAll());
-
-delite.forEach((item,i)=>{
-    item.addEventListener('click', () =>{
-        counter = counter - 1 ;
-        createCount(counter );
-        for(let i = 0 ; i<= task.length;i++){
-            task[i].innerHTML = task[i + 1].innerHTML;
-            if (i === 2){
-                task[3].innerHTML= task[4].innerHTML;
-            }else if ( i === 3){
-                task[task.length - 1].innerHTML = ``;
-            }  
-        }
-       
-    })
-})
-
-function addTask(count){
-    if(counter > 5){
-       alert('maybe that is it for today ?')
-       counter = 5;
-    }else{
-        task[count].innerHTML= `<h3> ${val.value}</h3>`;
-        val.value = '';
-    }
-}
-
-function deliteAll(){
-    counter = 0;
-    createCount(counter);
-    for(let i = 0 ; i <= task.length ; i++){
-        task[i].innerHTML= ``;
-    } 
-}
 
 
 
